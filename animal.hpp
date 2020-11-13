84b5e3e73af4360d5d6c53b004bafb6234a70580
Allan Christian Dantas Gomes <74272890+allancdg@users.noreply.github.com>
2020-11-13 06:46:58 -0300

    Add via upload Chrome
    Enviando arquivo inicial (Header) da Classe "Animal" do PetFera

---
+++ animal.hpp
@@ -0,0 +1,39 @@
+#pragma once        //Função semelhante ao Define, c++11
+
+#include <iostream>
+
+using namespace std;
+
+class Animal{               //Criação da classe Animal (Geral à todas as familias de animais);
+protected:
+    int id;                 //Código identificador do animal no PetFera;
+    string nome;            // Nome como o animal é conhecido popularmente; "Jararaca";
+    string nome_cientifico; // Nome cientifico do animal; "Bothrops jararaca";
+    string nome_batismo;    // Nome de batismo do animal; "Joãozinho";
+    string classe;          // Classe do animal; "Amphibia, Reptilia, Aves ou Mammalithis"
+    char sexo;              // Macho, fêmea;
+    float tamanho;          // Tamanho do animal, em metros, ao ser cadastrado no PetFera;
+    string dieta;           // Dieta predominante do animal;
+
+public:
+    //Construtor padrão;
+    Animal();
+    //Construtor parametrizado;
+    Animal(int id, string nome, string nome_cientifico, string nome_batismo, string classe, char sexo,
+           float tamanho, string dieta);
+    //Destrutor padrão;
+    ~Animal();
+
+    //Getters
+    int get_id() const;
+
+    //Setters
+    void set_id( int id );
+    void set_nome(string nome);
+    void set_nome_cientifico( string nome_cientifico );
+    void set_nome_batismo(string nome_batismo);
+    void set_classe( string classe );
+	void set_sexo( char sexo );
+	void set_tamanho( float tamanho);
+	void set_dieta( string dieta );
+};
